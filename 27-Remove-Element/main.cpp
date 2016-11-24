47ac0a9e325c48da16875eb3ccd1f829ae354f90
scloudyy <onecloud.shen@gmail.com>
2016-11-24 17:41:10 +0800

    add 2 more methods to problem 27

--- 27-Remove-Element/main.cpp
+++ 27-Remove-Element/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 	public:
-	int removeElement(vector<int> &nums, int val) {
+	int removeElement01(vector<int>& nums, int val) {
+		// 使用迭代器
 		vector<int>::iterator it;
 		for(it = nums.begin(); it != nums.end();) {
 			if (*it == val)
@@ -15,7 +17,26 @@ class Solution {
 		}
 		return nums.size();
 	}
-	
+
+	int removeElement02(vector<int>& nums, int val) {
+		// 使用数组的方式
+		int index = 0;
+		for (int i = 0; i < nums.size(); ++i)
+			if (nums[i] != val)
+				nums[index++] = nums[i];
+		return index;
+	}
+
+	int removeElement03(vector<int>& nums, int val) {
+		// 使用STL
+		auto iter = nums.end();
+    while((iter = find(nums.begin(), nums.end(), val)) != nums.end())
+        {
+            nums.erase(iter);
+        }
+    return nums.size();
+	}
+
 	vector<int> testcase() {
 		int arr[6] = {1, 2, 3, 4, 5, 6};
 		vector<int> vec(arr, arr + sizeof(arr) / sizeof(int));
@@ -24,10 +45,10 @@ class Solution {
 
 	void output(const vector<int> &nums, int size) {
 		int i = 0;
-		for(i; i < nums.size(); i++) {
+		for(i; i < size; i++) {
 			cout << nums[i] << " ";
 		}
-		cout << endl << "size: " << size;
+		cout << endl << "size: " << size << endl;
 	}
 };
 
@@ -38,7 +59,15 @@ int main() {
 	Solution* slt = new Solution();
 
 	vec = slt->testcase();
-	size = slt->removeElement(vec, 3);
+	size = slt->removeElement01(vec, 3);
+	slt->output(vec, size);
+
+	vec = slt->testcase();
+	size = slt->removeElement02(vec, 3);
+	slt->output(vec, size);
+
+	vec = slt->testcase();
+	size = slt->removeElement03(vec, 3);
 	slt->output(vec, size);
 
 	delete slt;
